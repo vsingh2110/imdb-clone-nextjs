e5e7fd8313326781ce119bf7392e42665064dbf7
Vikas Singh <vsingh2110@gmail.com>
2024-06-07 20:54:35 +0530

    final done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,25 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
-import Providers from "./Providers";
-import Navbar from "@/components/Navbar";
-import SearchBox from "@/components/SearchBox";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import Providers from './Providers';
+import Navbar from '@/components/Navbar';
+import SearchBox from '@/components/SearchBox';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "IMDB Clone Next.js App",
-  description:
-    "This is movie database clone made using Nextjs Javascript Framework",
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
         <Providers>
           <Header />
-          <Navbar/>
-          <SearchBox/>
+          <Navbar />
+          <SearchBox />
           {children}
         </Providers>
       </body>
