8ecfbd4ab42a4bba103b3093f57c0b1315f10a11
Vikas Singh <vsingh2110@gmail.com>
2024-06-07 20:26:29 +0530

    till homepage done

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     experimental: {
       missingSuspenseWithCSRBailout: false,
     },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'image.tmdb.org',
+          pathname: '**',
+        },
+      ],
+    },
   };
 
 export default nextConfig;
